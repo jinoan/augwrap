b26d9a49e68678eb76b1f48ee230aba5f7f91107
noah <noah@hyper-cloud.kr>
2021-12-10 14:47:35 +0900

    add RandomLocateInFrame

--- src/augwrap/augmentations.py
+++ src/augwrap/augmentations.py
@@ -1,7 +1,8 @@
 import os
+import json
 import random
 import numpy as np
-import json
+import cv2
 from PIL import Image
 import albumentations as A
 from copy import deepcopy
@@ -85,9 +86,14 @@ class MixUp(A.BasicTransform):
     def image_apply(self, image, **kwargs):
         index = random.randrange(self.dataset.__len__())
         self.sub_sample = self.dataset[index]
+        resize = A.Compose([
+            A.RandomSizedBBoxSafeCrop(*image.shape[:2])
+            ], bbox_params=A.BboxParams(format='albumentations', min_area=0.3, min_visibility=0.3, label_fields=['labels'])
+        )
+        self.sub_sample = resize(**self.sub_sample)
         self.r = random.uniform(*self.rate_range)
-        image = image * (1 - self.r) + 0.5 + self.sub_sample["image"] * self.r
-        image = np.int16(image)
+        image = image * (1 - self.r) + self.sub_sample["image"] * self.r
+        image = np.uint8(image)
         return image
 
     def label_apply(self, label, **kwargs):
@@ -131,6 +137,110 @@ class KelvinWB(A.ImageOnlyTransform):
         return image
 
 
+class RandomLocateInFrame(A.BasicTransform):
+    def __init__(
+        self,
+        h,
+        w,
+        interpolation=cv2.INTER_AREA,
+        border_mode=cv2.BORDER_REPLICATE,
+        always_apply=False,
+        p=1,
+    ):
+        super(RandomLocateInFrame, self).__init__(always_apply, p)
+        self.h = h
+        self.w = w
+        self.interpolation = interpolation
+        self.border_mode = border_mode
+
+    @property
+    def target_dependence(self):
+        return {'image': ['bboxes', 'labels'],
+                'bboxes': ['image'],
+                'labels': ['image', 'bboxes']}
+
+    def crop_side_with_safe_bbox(self, frame_side, img_side, safe_set):
+        side_min = random.choice(list(set(range(img_side - frame_side)) & safe_set))
+        side_max = side_min + frame_side + 1
+        while side_max < img_side:
+            if side_max in safe_set:
+                break
+            side_max += 1
+        return side_min, side_max
+            
+    def apply(self, image, **params):
+        img_h, img_w, _ = image.shape
+
+        h_set = set(range(img_h))
+        w_set = set(range(img_w))
+
+        for bbox in params['bboxes']:
+            w_set -= set(range(int(img_w * bbox[0] + 0.5), int(img_w * bbox[2] + 0.5) + 1))
+            h_set -= set(range(int(img_h * bbox[1] + 0.5), int(img_h * bbox[3] + 0.5) + 1))
+
+        # crop
+        if self.h < img_h:
+            self.img_h_min, self.img_h_max = self.crop_side_with_safe_bbox(self.h, img_h, h_set)
+        else:
+            self.img_h_min, self.img_h_max = 0, img_h
+
+        if self.w < img_w:
+            self.img_w_min, self.img_w_max = self.crop_side_with_safe_bbox(self.w, img_w, w_set)
+        else:
+            self.img_w_min, self.img_w_max = 0, img_w
+        
+        img = image[self.img_h_min:self.img_h_max, self.img_w_min:self.img_w_max, ...]
+
+        # resize and locate
+        img_h, img_w, _ = img.shape
+        if self.h / img_h < self.w / img_w:
+            self.r = self.h / img_h
+        else:
+            self.r = self.w / img_w
+
+        self.h_min = random.randrange(self.h - int(img_h * self.r + 0.5) + 1)
+        self.w_min = random.randrange(self.w - int(img_w * self.r + 0.5) + 1)
+
+        mtrx = np.array(
+            [[self.r, 0, self.w_min],
+             [0, self.r, self.h_min]]
+        , dtype=np.float32)
+
+        img = cv2.warpAffine(img, mtrx, (self.w, self.h), flags=self.interpolation, borderMode=self.border_mode)
+
+        return img
+
+    def apply_to_bboxes(self, bboxes, **params):
+        new_bboxes = []
+        img_h, img_w, _ = params['image'].shape
+        for bbox in bboxes:
+            x_min, y_min, x_max, y_max = bbox[0] * img_w, bbox[1] * img_h, bbox[2] * img_w, bbox[3] * img_h
+            if x_min >= self.img_w_min and y_min >= self.img_h_min and x_max < self.img_w_max and y_max < self.img_h_max:
+                new_bbox = (
+                    ((x_min - self.img_w_min) * self.r + self.w_min) / self.w,
+                    ((y_min - self.img_h_min) * self.r + self.h_min) / self.h,
+                    ((x_max - self.img_w_min) * self.r + self.w_min) / self.w,
+                    ((y_max - self.img_h_min) * self.r + self.h_min) / self.h,
+                    bbox[-1]
+                    )
+                new_bboxes.append(new_bbox)
+        return new_bboxes
+
+    def apply_to_labels(self, labels, **params):
+        new_labels = []
+        img_h, img_w, _ = params['image'].shape
+        for bbox, label in zip(params['bboxes'], labels):
+            x_min, y_min, x_max, y_max = bbox[0] * img_w, bbox[1] * img_h, bbox[2] * img_w, bbox[3] * img_h
+            if x_min >= self.img_w_min and y_min >= self.img_h_min and x_max < self.img_w_max and y_max < self.img_h_max:
+                new_labels.append(label)
+        
+        return new_labels
+
+    @property
+    def targets(self):
+        return {'image': self.apply, 'bboxes': self.apply_to_bboxes, 'labels': self.apply_to_labels}
+
+
 class Mosaic(A.BasicTransform):
     def __init__(
         self,
@@ -148,7 +258,7 @@ class Mosaic(A.BasicTransform):
         self.width_split_range = width_split_range
         if bbox_params is None:
             bbox_params = A.BboxParams(format=dataset.bbox_format, min_area=0.3, min_visibility=0.3, label_fields=['labels'])
-        self.transforms = A.Compose(transforms, bbox_params=bbox_params)
+        self.transforms = transforms
         self.bbox_params = bbox_params
 
     @property
@@ -156,12 +266,9 @@ class Mosaic(A.BasicTransform):
         return {'image': ['bboxes', 'labels']}
 
     def get_piece(self, data, h, w):
-        data = self.transforms(**data)
-        height, width = data['image'].shape[:2]
-        if height >= h and width >= w:
-            data = A.Compose([A.RandomCrop(always_apply=True, height=h, width=w)], bbox_params=self.bbox_params)(**data)
-        else:
-            data = A.Compose([A.Resize(always_apply=True, height=h, width=w)], bbox_params=self.bbox_params)(**data)
+        transforms = self.transforms + [RandomLocateInFrame(h, w, always_apply=True)]
+        annotation = A.Compose(transforms, bbox_params=self.bbox_params)
+        data = annotation(**data)
         return data
 
     def locate_bboxes(self, piece, h_r, w_r, h_p, w_p):
